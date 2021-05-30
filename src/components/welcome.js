66fffd74574214fa842f72d10efac72be6205ad5
Rafael <37466798+likeavenus@users.noreply.github.com>
2021-05-30 16:40:19 +0300

    develop: add welcome, popup, training

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,1 +1,16 @@
-export default function Welcome () {}
+export default function Welcome () {
+    const buttonPopupElem = document.querySelector('.js-open-popup');
+    const mainElem = document.querySelector('.main');
+    const popupElem = document.querySelector('.js-popup');
+    const closeButtonElem = document.querySelector('.button__close');
+
+    buttonPopupElem.addEventListener('click', () => {
+        mainElem.classList.add('main--blured');
+        popupElem.classList.add('popup--active');
+    });
+
+    closeButtonElem.addEventListener('click', () => {
+        mainElem.classList.remove('main--blured');
+        popupElem.classList.remove('popup--active');
+    });
+}
