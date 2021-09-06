07b444221ccf0a1f814b93be2bd01117fe7a618d
Rafael <37466798+likeavenus@users.noreply.github.com>
2021-09-06 13:47:37 +0300

    develop: fix arrows

--- src/components/slider.js
+++ src/components/slider.js
@@ -1,4 +1,4 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination, beforeSlideChangeStart } from 'swiper';
 
 Swiper.use([Navigation, Pagination]);
 
@@ -6,6 +6,7 @@ export default function Slider() {
     const swiperReviews = new Swiper('.swiper-container', {
         direction: 'horizontal',
         loop: false,
+        calculateHeight:true,
         pagination: {
           el: '.swiper-pagination',
           clickable: true,
@@ -14,6 +15,20 @@ export default function Slider() {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
         },
+        on: {
+          slideChangeTransitionStart: function () {
+            const currentSlide = document.querySelector('.swiper-slide.swiper-slide-active .slider__slide');
+            if (currentSlide) {
+              document.querySelector('.reviews .swiper-wrapper').style.height = `${currentSlide.offsetHeight}px`;
+            }
+            
+          },
+          beforeInit: function() {
+            const currentSlide = document.querySelector('.swiper-slide.swiper-slide-active .slider__slide');
+            if (!currentSlide && window.innerWidth <= 1280) {
+              document.querySelector('.reviews .swiper-wrapper').style.height = `520px`;
+            }
+          }
+        },
     });
-
 };
