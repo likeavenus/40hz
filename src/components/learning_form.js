5dfc623afb0dcf7fe3c6c96ac1b0e6e26507d9bd
Rafael <37466798+likeavenus@users.noreply.github.com>
2022-01-21 13:27:02 +0300

    develop: add validation

--- src/components/learning_form.js
+++ src/components/learning_form.js
@@ -3,21 +3,35 @@ export default function LearningForm() {
 	const learningForm = document.querySelector('.about__form');
 	const formPopup = document.querySelector('.contact__popup');
 	const formPopupTitle = document.querySelector('.contact__popup-title');
+	const nameInput = document.querySelector('#name');
+	const phoneInput = document.querySelector('#phone');
+	const mailInput = document.querySelector('#mail');
+	const sendBtn = document.querySelector('.send__about');
+
+	const mailRegEx = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
+	learningForm.addEventListener('input', () => {
+		console.log(mailRegEx.test(mailInput.value))
+		if (nameInput.value && phoneInput.value && mailRegEx.test(mailInput.value)) {
+			sendBtn.removeAttribute('disabled');
+		} else {
+			sendBtn.setAttribute('disabled', true);
+		}
+	})
 
 	learningFormBtn.addEventListener('click', async (e) => {
-
 		e.preventDefault();
 		const response = await fetch('/learning.php', {
 			method: 'POST',
 			body: new FormData(learningForm)
 		});
-		learningForm.reset();
 
 		const result = await response;
 		if (result.ok) {
 			formPopupTitle.innerText = 'Спасибо! Мы скоро свяжемся с вами!';
 			fbq('track', 'Lead');
 			formPopup.classList.add('active');
+			learningForm.reset();
 			setTimeout(() => {
 				formPopup.classList.remove('active');
 				formPopupTitle.innerText = '';
